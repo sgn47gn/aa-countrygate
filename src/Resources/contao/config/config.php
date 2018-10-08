1a2b911dd70a17a919b458aefdcf22ef29f252f5
Holger Neuner <neuner@47gradnord.de>
2018-10-08 10:01:25 +0200

    Test

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * GIT SCHRANKEN-MODUL for Contao Open Source CMS
+ *
+ * Copyright (C) 2018 47GradNord - Agentur für Internetlösungen
+ *
+ * @license    commercial
+ * @author     Holger Neuner
+ */
+
+/*
+ * Frontend Module
+ */
+$GLOBALS['FE_MOD']['countrygateway'] = [
+    'cw_test' => 'sgn47gn\Countrygate\Module\Test',
+];
