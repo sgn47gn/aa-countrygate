1c1a984bbb9828f5e19be17b2837c4f9f53f2424
Holger Neuner <neuner@47gradnord.de>
2018-10-08 09:45:48 +0200

    initial Commit

---
+++ src/CountrygateBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * GIT SCHRANKEN-MODUL for Contao Open Source CMS
+ *
+ * Copyright (C) 2018 47GradNord - Agentur für Internetlösungen
+ *
+ * @license    commercial
+ * @author     Holger Neuner
+ */
+
+
+namespace sgn47gn\Countrygate;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CountrygateBundle extends Bundle
+{
+
+}
